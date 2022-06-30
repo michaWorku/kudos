fff9af20881d94405f6c5ca2e5f318ec040dcaf7
mikiasW <mikias27worku@gmail.com>
2022-06-30 17:26:20 +0300

    add register user - check if user exists with provided email - create user or return error response

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -1,11 +1,26 @@
+// app/utils/auth.server.ts
 
+import { json } from '@remix-run/node'
 import type { RegisterForm } from './types.server'
 import { prisma } from './prisma.server'
-import { json } from '@remix-run/node'
+import { createUser } from './users.server'
+
 
 export async function register(user: RegisterForm) {
   const exists = await prisma.user.count({ where: { email: user.email } })
   if (exists) {
     return json({ error: `User already exists with that email` }, { status: 400 })
   }
+
+
+ const newUser = await createUser(user)
+ if (!newUser) {
+   return json(
+     {
+       error: `Something went wrong trying to create a new user.`,
+       fields: { email: user.email, password: user.password },
+     },
+     { status: 400 },
+   )
+ }
 }
