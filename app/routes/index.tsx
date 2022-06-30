4c66cdb6d0b10b7a0586ab896b3c63594988faf2
mikiasW <mikias27worku@gmail.com>
2022-06-30 15:52:34 +0300

    Remix project setup

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="h-screen w-full bg-slate">
+      <h1 className="font-bold text-5xl text-blue-600">Tailwind css works fine</h1>
+    </div>
+  );
+}
