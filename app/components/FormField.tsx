ca68356e0f2607717c7e9ce67dce55f18867014f
mikiasW <mikias27worku@gmail.com>
2022-06-30 20:47:53 +0300

    add validation for form fileld

--- app/components/FormField.tsx
+++ app/components/FormField.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 
 interface FormFieldProps {
     htmlFor: string
@@ -5,22 +6,27 @@ interface FormFieldProps {
     type?: string
     value: any
     onChange?: (...args: any) => any
+    error?: string
   }
   
-export function FormField({ htmlFor, label, type = 'text', value, onChange = () => {} }: FormFieldProps) {
+export function FormField({ htmlFor, label, type = 'text', value, onChange = () => {}, error = "" }: FormFieldProps) {
+    const [errorText, setErrorText] = useState(error)
+    useEffect(() => {
+        setErrorText(error)
+    }, [error])
+
 return (
     <>
         <label htmlFor={htmlFor} className="text-blue-600 font-semibold">
             {label}
         </label>
-        <input
-            onChange={onChange}
-            type={type}
-            id={htmlFor}
-            name={htmlFor}
-            className="w-full p-2 rounded-xl my-2"
-            value={value}
-        />
+        <input onChange={e => {
+            onChange(e)
+            setErrorText('')
+        }} type={type} id={htmlFor} name={htmlFor} className="w-full p-2 rounded-xl my-2" value={value} />
+        <div className="text-xs font-semibold text-center tracking-wide text-red-500 w-full">
+            {errorText || ''}
+        </div>
     </>
 )
 }
