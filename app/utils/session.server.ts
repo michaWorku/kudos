2fa2e7b7c50d92b352b547eee6467a7e23393399
mikiasW <mikias27worku@gmail.com>
2022-06-30 18:48:20 +0300

    git user id and logout session utils

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -1,4 +1,5 @@
 import { json, createCookieSessionStorage, redirect } from '@remix-run/node'
+import { prisma } from './prisma.server'
 
 const sessionSecret = process.env.SESSION_SECRET
 if (!sessionSecret) {
@@ -26,3 +27,34 @@ export async function createUserSession(userId: string, redirectTo: string) {
       },
     })
   }
+
+export async function requireUserId(request: Request, redirectTo: string = new URL(request.url).pathname) {
+  const session = await getUserSession(request)
+  const userId = session.get('userId')
+  if (!userId || typeof userId !== 'string') {
+    const searchParams = new URLSearchParams([['redirectTo', redirectTo]])
+    throw redirect(`/login?${searchParams}`)
+  }
+  return userId
+}
+
+function getUserSession(request: Request) {
+  return storage.getSession(request.headers.get('Cookie'))
+}
+
+export async function getUserId(request: Request) {
+  const session = await getUserSession(request)
+  const userId = session.get('userId')
+  if (!userId || typeof userId !== 'string') return null
+  return userId
+}
+
+
+export async function logout(request: Request) {
+  const session = await getUserSession(request)
+  return redirect('/login', {
+    headers: {
+      'Set-Cookie': await storage.destroySession(session),
+    },
+  })
+}
