86470ee30ea0467aeb09fa9a31c90d6945e06f96
mikiasW <mikias27worku@gmail.com>
2022-06-30 18:17:33 +0300

    add toggling button

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -69,6 +69,12 @@ export default function Login() {
   return (
     <Layout>
       <div className="h-full justify-center items-center flex flex-col gap-y-4">
+        <button
+          onClick={() => setAction(action == 'login' ? 'register' : 'login')}
+          className="absolute top-8 right-8 rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+        >
+          {action === 'login' ? 'Sign Up' : 'Sign In'}
+      </button>
         <h2 className="text-5xl font-extrabold text-yellow-300">Welcome to Kudos!</h2>
         <p className="font-semibold text-slate-300">
           {action === 'login' ? 'Log In To Give Some Praise!' : 'Sign Up To Get Started!'}
