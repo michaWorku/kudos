8ba07c5775d2577b677af83d74be1eb3e7c5b6b0
mikiasW <mikias27worku@gmail.com>
2022-06-30 16:07:19 +0300

    Create a reusable layout component

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,8 +1,9 @@
+import { Layout } from "~/components/Layout"
 
 export default function Login() {
   return (
-    <div className="h-screen flex justify-center items-center">
+    <Layout>
       <h2 className="text-yellow-300 font-extrabold text-5xl">Login Route</h2>
-    </div>
+    </Layout>
   )
 }
