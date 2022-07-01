22d1f7203bd82babb9ab2c8712448b35b5e04b44
mikiasW <mikias27worku@gmail.com>
2022-07-01 13:56:08 +0300

    Add Get filterd kudos
    - get kudos of user
    - sort and filter the result

--- app/utils/kudo.server.ts
+++ app/utils/kudo.server.ts
@@ -1,5 +1,5 @@
 import { prisma } from './prisma.server'
-import { KudoStyle } from '@prisma/client'
+import { KudoStyle, Prisma } from '@prisma/client'
 
 export const createKudo = async (message: string, userId: string, recipientId: string, style: KudoStyle) => {
   await prisma.kudo.create({
@@ -21,4 +21,30 @@ export const createKudo = async (message: string, userId: string, recipientId: s
       },
     },
   })
+}
+
+export const getFilteredKudos = async (
+    userId: string,
+    sortFilter: Prisma.KudoOrderByWithRelationInput,
+    whereFilter: Prisma.KudoWhereInput,
+  ) => {
+    return await prisma.kudo.findMany({
+      select: {
+        id: true,
+        style: true,
+        message: true,
+        author: {
+          select: {
+            profile: true,
+          },
+        },
+      },
+      orderBy: {
+        ...sortFilter,
+      },
+      where: {
+        recipientId: userId,
+        ...whereFilter,
+      },
+    })
 }
