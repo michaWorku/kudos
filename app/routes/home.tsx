e1869e60ca025f9e53c6528548eb4541979d0bb3
mikiasW <mikias27worku@gmail.com>
2022-07-01 15:30:05 +0300

    provide user data to search bar component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Outlet, useLoaderData } from '@remix-run/react'
 import { Layout } from '~/components/Layout'
 import { UserPanel } from '~/components/UserPanel'
 import { requireUserId } from '~/utils/session.server'
-import { getOtherUsers } from '~/utils/users.server'
+import { getOtherUsers, getUser } from '~/utils/users.server'
 import { Kudo as IKudo, Prisma, Profile } from '@prisma/client'
 import { getFilteredKudos, getRecentKudos } from '~/utils/kudo.server'
 import { Kudo } from '~/components/Kudo'
@@ -61,19 +61,21 @@ export const loader: LoaderFunction = async ({ request }) => {
  const kudos = await getFilteredKudos(userId, sortOptions, textFilter)
  
  const recentKudos = await getRecentKudos()
+
+ const user = await getUser(request)
   
- return json({ users, kudos, recentKudos })
+ return json({ users, kudos, recentKudos, user })
 }
 
 export default function Home() {
-  const { users, kudos, recentKudos } = useLoaderData()
+  const { users, kudos, recentKudos, user } = useLoaderData()
   return (
     <Layout>
       <Outlet />
       <div className="h-full flex">
         <UserPanel users={users} />
         <div className="flex-1 flex flex-col">
-          <SearchBar />
+          <SearchBar profile={user.profile}/>
           <div className="flex-1 flex">
             <div className="w-full p-10 flex flex-col gap-y-4">
               {kudos.map((kudo: KudoWithProfile) => (
