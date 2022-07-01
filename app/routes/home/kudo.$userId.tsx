174b9334b4edeac723000e380d1bb14190018f6c
mikiasW <mikias27worku@gmail.com>
2022-07-01 12:03:13 +0300

    Render select-box with possible options

--- app/routes/home/kudo.$userId.tsx
+++ app/routes/home/kudo.$userId.tsx
@@ -4,7 +4,9 @@ import { useActionData, useLoaderData } from '@remix-run/react'
 import { useState } from 'react'
 import { Modal } from '~/components/Modal'
 import { Portal } from '~/components/Portal'
+import { SelectBox } from '~/components/SelectBox'
 import { UserCircle } from '~/components/UserCircle'
+import { colorMap, emojiMap } from '~/utils/constants'
 import { getUser, getUserById } from '~/utils/users.server'
 
 
@@ -38,6 +40,27 @@ const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElemen
   setFormData(data => ({ ...data, [field]: e.target.value }))
 }
 
+const handleStyleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, field: string) => {
+  setFormData(data => ({
+      ...data, style: {
+          ...data.style,
+          [field]: e.target.value
+      }
+  }))
+}
+
+const getOptions = (data: any) => Object.keys(data).reduce((acc: any[], curr) => {
+  acc.push({
+      name: curr.charAt(0).toUpperCase() + curr.slice(1).toLowerCase(),
+      value: curr
+  })
+  return acc
+}, [])
+
+const colors = getOptions(colorMap)
+const emojis = getOptions(emojiMap)
+
+
   const { 
     recipient, 
     user 
@@ -70,7 +93,33 @@ const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElemen
               placeholder={`Say something nice about ${recipient.profile.firstName}...`}
             />
             <div className="flex flex-col items-center md:flex-row md:justify-start gap-x-4">
-              {/* Select Boxes Go Here */}
+              <SelectBox
+                  options={colors}
+                  name="backgroundColor"
+                  value={formData.style.backgroundColor}
+                  onChange={e => handleStyleChange(e, 'backgroundColor')}
+                  label="Background Color"
+                  containerClassName="w-36"
+                  className="w-full rounded-xl px-3 py-2 text-gray-400"
+              />
+              <SelectBox
+                  options={colors}
+                  name="textColor"
+                  value={formData.style.textColor}
+                  onChange={e => handleStyleChange(e, 'textColor')}
+                  label="Text Color"
+                  containerClassName="w-36"
+                  className="w-full rounded-xl px-3 py-2 text-gray-400"
+              />
+              <SelectBox
+                  options={emojis}
+                  label="Emoji"
+                  name="emoji"
+                  value={formData.style.emoji}
+                  onChange={e => handleStyleChange(e, 'emoji')}
+                  containerClassName="w-36"
+                  className="w-full rounded-xl px-3 py-2 text-gray-400"
+              />
             </div>
           </div>
         </div>
